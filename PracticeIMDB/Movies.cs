eee1a5d1748ac71cc3450da30376e61c46d84bbb
Kaan Kaya <kaankaya.0707@gmail.com>
2024-08-12 02:22:23 +0300

    Add project files.

---
+++ PracticeIMDB/Movies.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeIMDB
+{
+    public class Movies
+    {
+        public string Name { get; set; }
+        public double Point { get; set; }
+
+        public Movies(string name, double point)
+        {
+            Name = name;
+            Point = point;
+        }
+
+        public void DisplayMovie()
+        {
+            Console.WriteLine("*********************");
+            Console.WriteLine($"Flim Adı : {Name} - IMDB Puanı : {Point}");
+        }
+    }
+}
---
+++ PracticeIMDB/Program.cs
@@ -0,0 +1,52 @@
+﻿using PracticeIMDB;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //
+        List<Movies> movieList = new List<Movies>();
+        string continueInput;
+        do
+        {
+            Console.WriteLine("Lütfen Flim Adını Giriniz");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Lütfen IMDB Puanı Giriniz");
+            double point;
+            while (!double.TryParse(Console.ReadLine(), out point))
+            {
+                Console.WriteLine("Lütfen Geçerli Bir puan Giriniz");
+            }
+            // Yeni bir Movie nesnesi oluştur ve listeye ekle
+            Movies movie = new Movies(name, point);
+            movieList.Add(movie);
+
+            Console.WriteLine("Başka Bir Flim Eklemek İstermisiniz Evet/Hayır");
+            continueInput = Console.ReadLine().ToLower();
+        } while (continueInput == "evet");
+            
+        //Listele
+        foreach(var movies in movieList)
+        {
+            movies.DisplayMovie();
+        }
+
+        // IMDB Puanı 4 ile 9 Arasında Olan Filmleri Listele
+        Console.WriteLine("\nIMDB Puanı 4 ile 9 Arasında Olan Filmler:");
+        var filteredMovies = movieList.Where(m => m.Point >= 4 && m.Point <= 9);
+        foreach (var movie in filteredMovies)
+        {
+            movie.DisplayMovie();
+        }
+
+        // İsmi 'A' ile Başlayan Filmler ve IMDB Puanları
+        Console.WriteLine("\nİsmi 'A' ile Başlayan Filmler ve IMDB Puanları:");
+        var moviesStartingWithA = movieList.Where(m => m.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase));
+        foreach (var movie in moviesStartingWithA)
+        {
+            movie.DisplayMovie();
+        }
+
+    }
+}
